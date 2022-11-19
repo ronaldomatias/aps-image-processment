2b2e47a4bbc2ddc48d8654067d9d71421daf7f9e
ronaldomatias <ronaldomts476@gmail.com>
2022-11-19 20:45:19 -0300

    feat: add fps on frame

--- initializer/run.py
+++ initializer/run.py
@@ -3,20 +3,19 @@ import apiconsumer.apiconsumer as api_consumer
 import util.drawerutil as drawer
 import time
 from datetime import datetime
+import uuid
 
 
-def init(soma=None):
+def init():
     send_message = True
     timer = 0
 
-    capture_service = cv2.VideoCapture("cut.mp4")
+    capture_service = cv2.VideoCapture("videos/soldados.mp4")
 
     net = cv2.dnn.readNet("config/yolov3.weights", "config/yolo.cfg")
     model = cv2.dnn_DetectionModel(net)
     model.setInputParams(size=(320, 320), scale=1 / 255)  # size precisa ser múltiplo de 32.
 
-    contador = 0
-    acumulador = 0
     while cv2.waitKey(1) != 27:
 
         frame_exists, frame = capture_service.read()
@@ -28,16 +27,14 @@ def init(soma=None):
 
         for (classid, acuracia, box) in zip(classes, acuracias, boxes):
             print(acuracia)
-            acumulador += acuracia
-            contador += 1
             box_label = f"gun: {acuracia}"
             drawer.draw_rectangle(frame, box, box_label)
 
             if (acuracia > 0.75) & send_message:
                 date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-                imagePath = f"/home/ronaldo/PycharmProjects/log-images/{str(date)}.jpg"
+                imagePath = f"/home/ronaldo/PycharmProjects/log-images/{str(uuid.uuid1())}.jpg"
                 cv2.imwrite(imagePath, frame)
-                api_consumer.comunicateApi(imagePath, date)
+                #api_consumer.comunicateApi(imagePath, date)
                 send_message = False
 
         # A CADA 5 MINUTOS É LIBERADO O ENVIO DE UMA NOVA MENSAGEM
@@ -45,10 +42,9 @@ def init(soma=None):
             timer = 0
             send_message = True
 
+        drawer.draw_fps(frame, end_time, start_time)
         cv2.imshow("Webcam", frame)
 
 
     capture_service.release()
     cv2.destroyAllWindows()
-    avg_acuracia = acumulador / contador
-    print(f"Media de certeza: {avg_acuracia}")
--- util/drawerutil.py
+++ util/drawerutil.py
@@ -3,8 +3,13 @@ import cv2
 def draw_rectangle(frame, box, label):
 
     color = (0, 0, 255)
-    # DESENHAR A BOX DA DETECÇÃO
+    # DESENHAR A CAIXA DE DETECÇÃO
     cv2.rectangle(frame, box, color, 2)
-
-    # ESCREVER O NOME DA CLASSE EM CIMA DA BOX DO OBJETO
+    # DESENHAR O NOME DA CLASSE
     cv2.putText(frame, label, (box[0], box[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+def draw_fps(frame, end_time, start_time):
+    color = (0, 255, 255)
+    fps = f"FPS: {round((1.0 / (end_time - start_time)), 2)}"
+    cv2.putText(frame, fps, (0, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 3)
